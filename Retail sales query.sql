f5428e9393de04fc0b2a5bb6b8f43009de064adc
mpumimoropa-dot <mpumi.moropa@gmail.com>
2026-06-08 13:12:09 +0000

    uploading sql code

---
+++ Retail sales query.sql
@@ -0,0 +1,16 @@
+-- Databricks notebook source
+USE brightl.data;
+
+-- COMMAND ----------
+
+USE brightl.data;
+
+-- COMMAND ----------
+
+--Runnong the entire table
+SELECT *
+FROM retail_sales;
+
+-- COMMAND ----------
+
+
