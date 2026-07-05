a487c2f0d8f69543a22a5ae0f632af4edb190ae0
mpumimoropa-dot <mpumi.moropa@gmail.com>
2026-07-05 14:18:38 +0000

    Save the latest query

--- Bright Cofee Shop.sql
+++ Bright Cofee Shop.sql
@@ -4,15 +4,15 @@
 -- COMMAND ----------
 
 ---UPLOADING THE TABLE
-SELECT *
-FROM brightcoffee.shop.coffee_sales_v
+SELECT*
+FROM brightcoffee.default.brightsales
 
 --Counting the number of rows
 SELECT COUNT(*)
-FROM brightcoffee.shop.coffee_sales_v
+FROM brightcoffee.default.brightsales
 
 ---descriping the data types and checking the nulls 
-DESCRIBE TABLE brightcoffee.shop.coffee_sales_v
+DESCRIBE TABLE brightcoffee.default.brightsales
 
 ---Finding rows containing IN Colmns 
 SELECT* 
@@ -42,15 +42,40 @@ SELECT product_id,product_type,product_detail,unit_price,
     IFNULL(unit_price, 0) AS price_verified
 FROM brightcoffee.shop.coffee_sales_v;
 
+---Store location
+SELECT DISTINCT store_location
+FROM brightcoffee.default.brightsales
+
+-- corrected format string
+ SELECT transaction_id,
+        transaction_date,
+        DATE_FORMAT(transaction_time,'HH:MM:SS') AS clean_time,
+        transaction_qty,
+        store_id,
+        store_location,
+    FROM brightcoffee.default.brightsales
+
 --data range of data set
 SELECT MIN(transaction_date) AS earliest_date,
        MAX(transaction_date) AS latest_date
-FROM brightcoffee.shop.coffee_sales_v;
+FROM brightcoffee.default.brightsales
 
 ---remove time stamp
 SELECT transaction_time,
-       DATE_FORMAT(transaction_time,'hh:mm:ss') AS clean_time
-  FROM brightcoffee.shop.coffee_sales_v;   
+       DATE_FORMAT(transaction_time,'hh:mm:ss') AS clean_time;
+ FROM brightcoffee.default.brightsales 
+
+ SELECT 
+    CASE 
+        WHEN DAYNAME(transaction_date) IN ('Saturday', 'Sunday') THEN 'Weekend'
+        ELSE 'Weekday'
+    END AS day_type,
+    COUNT(*) AS transaction_count,                     -- number of transactions
+    SUM(CAST(transaction_qty AS DOUBLE) 
+        * CAST(REPLACE(unit_price, ',', '') AS DOUBLE)) AS total_revenue  -- total revenue
+FROM brightcoffee.shop.coffee_sales_v
+GROUP BY day_type;
+
 
   SELECT *
 FROM brightcoffee.shop.coffee_sales_v
@@ -69,15 +94,45 @@ FROM brightcoffee.shop.coffee_sales_v
             WHEN EXTRACT(HOUR FROM transaction_time) BETWEEN 15 AND 17 THEN 'Afternoon'
             ELSE 'Evening'
         END AS time_period,
+    END AS month_period
         (store_id * unit_price) AS total_sales
     FROM brightcoffee.shop.coffee_sales_v
-)
-SELECT 
+
+     SELECT
+        CASE 
+            WHEN DAYNAME(transaction_date) AS day_name, 
+            WHEN DAYNAME(transaction_date) IN ('Saturday', 'Sunday') THEN 'Weekend'
+        ELSE 'Weekday'
+    END AS day_type
+    FROM brightcoffee.default.brightsales;
+
+SELECT
     time_period,
     COUNT(*) AS transaction_count,
     SUM(total_sales) AS grand_total
-FROM sales_with_period
+FROM brightcoffee.shop.coffee_sales_v;
 GROUP BY time_period;
+
+SELECT 
+    CASE
+        WHEN (CAST(transaction_qty AS DOUBLE) * CAST(REPLACE(unit_price, ',', '') AS DOUBLE)) <= 50 
+            THEN 'Cheap spend'
+        WHEN (CAST(transaction_qty AS DOUBLE) * CAST(REPLACE(unit_price, ',', '') AS DOUBLE)) BETWEEN 51 AND 200 
+            THEN 'Low spend'
+        WHEN (CAST(transaction_qty AS DOUBLE) * CAST(REPLACE(unit_price, ',', '') AS DOUBLE)) BETWEEN 201 AND 300 
+            THEN 'Medium spend'
+        ELSE 'Expensive spend'
+    END AS spend_bucket
+ 
+    CAST(REPLACE(unit_price, ',', '') AS DOUBLE) AS clean_unit_price,   -- clean numeric price
+    ROUND(
+        CAST(transaction_qty AS DOUBLE) * CAST(REPLACE(unit_price, ',', '') AS DOUBLE),
+        2
+    ) AS Revenue   -- revenue per row
+FROM brightcoffee.shop.coffee_sales_v;
+
+
+
 
 -------------------------------------------------------------------------------------------------------------------------
 Final query that brings master table 
@@ -85,30 +140,62 @@ Final query that brings master table
 SELECT *
  FROM brightcoffee.shop.coffee_sales_v
 
- SELECT transaction_id,
-        transaction_date,
-        DATE_FORMAT(transaction_time,'hh:mm:ss') AS clean_time,
-        transaction_qty,
-        store_id,
-        store_location,
-        product_id,
-        unit_price,
-        product_category,
-        product_type,
-        product_detail,
-        DAYNAME(transaction_date) AS day_name, ------day name (moday, tues...)
-        MONTHNAME(transaction_date) AS month_name, ---month name (Jan, Feb...)
-        DAYOFMONTH(transaction_date) AS day_number, ---day of month (1,2 ...)
-        CASE 
-            WHEN EXTRACT(HOUR FROM transaction_time) BETWEEN 6 AND 11 THEN 'Morning'
-            WHEN EXTRACT(HOUR FROM transaction_time) BETWEEN 12 AND 14 THEN 'Lunch'
-            WHEN EXTRACT(HOUR FROM transaction_time) BETWEEN 15 AND 17 THEN 'Afternoon'
-            ELSE 'Evening'
-        END AS time_bucket,
-        CASE 
-        WHEN EXTRACT(DOW FROM transaction_date) IN (0, 5) THEN 'Weekend'
-        ELSE 'Weekday'
-    END AS day_type
+ ---verifying if null in product category 
+SELECT 
+    product_id,product_category,product_type,product_detail,
+    IFNULL(product_category, 'n/a') AS product_categ_verified
 FROM brightcoffee.shop.coffee_sales_v;
+
+SELECT 
+    transaction_id,
+    transaction_date,
+    DATE_FORMAT(transaction_time,'HH:MM:SS') AS clean_time,  -- corrected format string
+    transaction_qty,
+    store_id,
+    store_location,
+    product_id,
+    unit_price,
+    product_category,
+    product_type,
+    product_detail,
+    DAYNAME(transaction_date) AS day_name,       -- day name (Monday, Tuesday...)
+    MONTHNAME(transaction_date) AS month_name,   -- month name (Jan, Feb...)
+    DAYOFMONTH(transaction_date) AS day_number,  -- day of month (1, 2 ...)
+   
+    CASE 
+        WHEN EXTRACT(HOUR FROM transaction_time) BETWEEN 6 AND 11 THEN 'Morning'
+        WHEN EXTRACT(HOUR FROM transaction_time) BETWEEN 12 AND 14 THEN 'Lunch'
+        WHEN EXTRACT(HOUR FROM transaction_time) BETWEEN 15 AND 17 THEN 'Afternoon'
+        ELSE 'Evening'
+    END AS time_bucket,
+    CASE 
+        WHEN DAYOFMONTH(transaction_date) BETWEEN 1 AND 10 THEN 'Early month' 
+        WHEN DAYOFMONTH(transaction_date) BETWEEN 11 AND 20 THEN 'Mid month'
+        ELSE 'Monthend'
+    END AS month_period,
+    
+    SELECT
+        CASE 
+           WHEN DAYNAME(transaction_date) IN ('Saturday', 'Sunday') THEN 'Weekend'
+        ELSE 'Weekday'
+        END AS day_type
+  
+    CASE
+        WHEN (CAST(transaction_qty AS DOUBLE) * CAST(REPLACE(unit_price, ',', '.') AS DOUBLE)) <= 50 
+            THEN 'Cheap spend'
+        WHEN (CAST(transaction_qty AS DOUBLE) * CAST(REPLACE(unit_price, ',', '.') AS DOUBLE)) BETWEEN 51 AND 200 
+            THEN 'Low spend'
+        WHEN (CAST(transaction_qty AS DOUBLE) * CAST(REPLACE(unit_price, ',', '.') AS DOUBLE)) BETWEEN 201 AND 300 
+            THEN 'Medium spend'
+        ELSE 'Expensive spend'
+        END AS spend_bucket,
+       
+
+        CAST(REPLACE(unit_price, ',', '.') AS DOUBLE) AS clean_unit_price,   -- clean numeric price
+        ROUND(CAST(transaction_qty AS DOUBLE) * CAST(REPLACE(unit_price, ',', '.') AS DOUBLE),2) AS Revenue   -- revenue per row
+FROM brightcoffee.default.brightsales;
+
+
+
 
 
